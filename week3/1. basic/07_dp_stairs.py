314fc3b5dc58643dab805d6d99c9755a1f9ad942
Junho Park <nari40@kyonggi.ac.kr>
2026-09-04 19:11:02 +0900

    또 다른 피보나치

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -46,15 +46,20 @@ def climb_stairs(n):
         n번째 계단까지 오르는 방법의 수
     """
     # TODO: 특별한 경우 처리
-    pass
-    
-    
     # TODO: dp 배열 생성 및 초기화
-    pass
-    
     # TODO: 작은 문제부터 차례로 계산
-    pass
-    
+    if n <= 2:
+        return n
+
+    dp = [0] * (n + 1)
+
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    # 4. n번째 결과 반환
     return dp[n]
 
 # 테스트 케이스
